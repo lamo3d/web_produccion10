7ce8ac67c6945b30a2fa1e0cdf41171185253683
Luis Merino <lamo3d@hotmail.com>
2015-06-18 15:57:06 -0500

    Revert "modificaciones lmo"
    This reverts commit b03768de580489a0c5f667d4966986e5913a45bb.

--- lmo_alertas_script.php
+++ lmo_alertas_script.php
@@ -1,8 +1,6 @@
 
 <?php
 
-//Script desarrollado por LAMO
-
 //require_once "/produccion/var/www/html/produccion/lmo_ewcfg10.php";
 //require_once "/produccion/var/www/html/produccion/lmo_ewmysql10.php";
 //require_once "/produccion/var/www/html/produccion/lmo_phpfn10.php";
@@ -23,11 +21,44 @@ set_time_limit(1000);
 //tarea correos
 //Enviar correos
 
+/*
+///Empezamos con el procedimiento de recuperación de una fila
+//Lo primero es crear un objeto mysqli
+$mysqli = new mysqli("localhost", "produccion", "produccion", "documentacion");
+
+/// comprobar conexión 
+if (mysqli_connect_errno()) {
+    printf("Conexión fallida: %s\n", mysqli_connect_error());
+    exit();
+}
+ 
+ 
+//Y llamamos al procedimiento para recoger los datos
+///Si falla imprimimos el error
+if (!$mysqli->multi_query("CALL pp_pa_alertas_correos(1)")) {
+    echo "Falló la llamada: (" . $mysqli->errno . ") " . $mysqli->error;
+}
+ 
+///Ahora con este bucle recogemos los resultados y los recorremos
+do {
+    //En el if recogemos una fila de la tabla
+    if ($res = $mysqli->store_result()) {
+        //Imprimimos el resultado de la fila y debajo un salto de línea
+        var_dump($res->fetch_all());
+        printf("\n");
+        //La llamada a free() no es obligatoria, pero si recomendable para aligerar memoria y 
+        //para evitar problemas si después hacemos una llamada a otro procedimiento
+        $res->free();
+    } else {
+        if ($mysqli->errno) {
+            echo "Store failed: (" . $mysqli->errno . ") " . $mysqli->error;
+        }
+    }
+} while ($mysqli->more_results() && $mysqli->next_result());
+//El bucle se ejecuta mientras haya más resultados y se pueda saltar al siguiente
 
 
-
-//Script desarrollado por LAMO
-
+*/
 
 $Conexion = new mysqli("localhost", "produccion", "produccion", "documentacion");
  if (mysqli_connect_errno()) {
